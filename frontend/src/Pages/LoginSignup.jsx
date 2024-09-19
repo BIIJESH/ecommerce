f33499fb4712389e32267c8818987d5e08753dd1
BIIJESH <BIIJESH@proton.me>
2024-09-19 14:29:17 +0545

    login pages needs to be refactored

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -1,26 +1,63 @@
-import React from "react";
-import './Css/LoginSignup.css'
+import React, { useState } from "react";
+import "./Css/LoginSignup.css";
 
 const LoginSignup = () => {
+  const [state, setState] = useState("Login");
   return (
     <div className="loginsignup">
       <div className="loginsignup-container">
-        <h1>Sign Up</h1>
+        <h1>{state}</h1>
         <div className="loginsignup-fields">
-          <input type="text" placeholder="Your Name" />
-          <input type="email" placeholder="Email Address" />
-          <input type="password" placeholder="Password" />
+          {state === "Sign Up" ? (
+            <input type="text" placeholder="Your name" name="username" />
+          ) : (
+            <></>
+          )}
+          <input type="email" placeholder="Email address" name="email" />
+          <input type="password" placeholder="Password" name="password" />
         </div>
-        <button>Continue</button>
-        <p className="loginsignup-login">Already have an account?</p>{" "}
-        <span>Login</span>
+
+        <button
+          onClick={() => {
+            state === "Login" ? login() : signup();
+          }}
+        >
+          Continue
+        </button>
+
+        {state === "Login" ? (
+          <p className="loginsignup-login">
+            Create an account?{" "}
+            <span
+              onClick={() => {
+                setState("Sign Up");
+              }}
+            >
+              Click here
+            </span>
+          </p>
+        ) : (
+          <p className="loginsignup-login">
+            Already have an account?{" "}
+            <span
+              onClick={() => {
+                setState("Login");
+              }}
+            >
+              Login here
+            </span>
+          </p>
+        )}
+
         <div className="loginsignup-agree">
           <input type="checkbox" name="" id="" />
-          <p>By continuing , I agree to the terms of use and privacy policy.</p>
+          <p>By continuing, i agree to the terms of use & privacy policy.</p>
         </div>
       </div>
     </div>
   );
+    //TODO:Make responsive in different resolution && 7:54:
+ 
 };
 
 export default LoginSignup;
